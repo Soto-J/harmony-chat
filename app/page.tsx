ad57bc72cd8e6b7491b1b90770dfaf7a858641a1
John Soto <soto.ja415@gmail.com>
2023-09-17 16:04:44 -0400

    Added Button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <p className="text-3xl font-bold text-indigo-500">Harmony</p>;
+  return <p className="text-3xl font-bold text-indigo-500">
+    <Button>Click me!</Button>
+    Harmony</p>;
 }
