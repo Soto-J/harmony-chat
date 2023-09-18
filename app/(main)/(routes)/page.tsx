37d17053115535a07ec49f2dc78f996e80c12812
john soto <johnsoto@johns-MacBook-Air.local>
2023-09-17 22:38:54 -0400

    Organized folder structure for auth and main

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return <div className="flex flex-col bg-red-100">
+    
+    This is a protected route</div>;
+}
