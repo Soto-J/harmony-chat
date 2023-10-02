953ce6580ea933d35a1904af0a22cbec60e20e91
John Soto <soto.ja415@gmail.com>
2023-10-02 18:56:45 -0400

    Added forced theme attribute to them provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,10 @@ export default function RootLayout({
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
+            forcedTheme="dark"
             enableSystem={false}
             disableTransitionOnChange
-            storageKey="discord-theme"
+            storageKey="harmony-theme"
           >
             {children}
           </ThemeProvider>
--- components/modals/inititial-modal.tsx
+++ components/modals/inititial-modal.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 import {
   Dialog,
   DialogContent,
@@ -7,9 +11,35 @@ import {
   DialogHeader,
   DialogTitle,
   DialogFooter,
-} from "../ui/dialog";
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  name: z.string().min(1, { message: "Server name is required" }),
+  imageUrl: z.string().min(1, { message: "Server image is required" }),
+});
 
 const InititialModal = () => {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
     <Dialog open>
       <DialogContent className="overflow-hidden bg-white p-0 text-black">
@@ -17,6 +47,10 @@ const InititialModal = () => {
           <DialogTitle className="text-center text-2xl font-bold">
             Customize your server
           </DialogTitle>
+          <DialogDescription className="text-center text-gray-500">
+            Give your server a personality with a name and an image. You can
+            always change it later.
+          </DialogDescription>
         </DialogHeader>
       </DialogContent>
     </Dialog>
