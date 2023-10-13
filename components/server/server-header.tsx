ee113fdd6cdb9247299ab4ed5c5417d802410341
John Soto <soto.ja415@gmail.com>
2023-10-12 23:53:51 -0400

    Added dropdownitems

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -3,9 +3,20 @@ import { MemberRole } from "@prisma/client";
 
 import {
   DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { ChevronDown } from "lucide-react";
+import {
+  ChevronDown,
+  LogOut,
+  PlusCircle,
+  Settings,
+  Trash,
+  UserPlus,
+  Users,
+} from "lucide-react";
 
 type ServerHeadderProps = {
   server: ServerWithChannelsAndMembers;
@@ -15,6 +26,20 @@ type ServerHeadderProps = {
 const ServerHeadder = ({ server, role }: ServerHeadderProps) => {
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
+
+  const dropDownItems = [
+    {
+      role: MemberRole.MODERATOR,
+      text: "Invite People",
+      icon: UserPlus,
+      fontPurple: true,
+    },
+    { role: MemberRole.ADMIN, text: "Server Settings", icon: Settings },
+    { role: MemberRole.ADMIN, text: "Manage Members", icon: Users },
+    { role: MemberRole.MODERATOR, text: "Create Channel", icon: PlusCircle },
+  ];
+
+  const moderatorItems = [];
 
   return (
     <DropdownMenu>
@@ -40,6 +65,85 @@ const ServerHeadder = ({ server, role }: ServerHeadderProps) => {
           <ChevronDown className="ml-auto h-5 w-5" />
         </button>
       </DropdownMenuTrigger>
+
+      <DropdownMenuContent
+        className="
+          w-56
+          space-y-[2px]
+          text-xs
+          font-medium
+          text-black
+          dark:text-neutral-400
+        "
+      >
+        {/* {dropDownItems.map(
+          (item) =>
+            role === "ADMIN" && (
+              <DropdownMenuItem
+                key={item.text}
+                className={`
+                  cursor-pointer 
+                  px-3 
+                  py-2 
+                  text-sm
+                  ${item.fontPurple && "text-indigo-600 dark:text-indigo-400"}
+                `}
+              >
+                {item.text}
+                <item.icon className="ml-auto h-4 w-4" />
+              </DropdownMenuItem>
+              
+            ),
+        )} */}
+        {isModerator && (
+          <DropdownMenuItem
+            className="
+              cursor-pointer
+              px-3
+              py-2
+              text-sm
+              text-indigo-600
+              dark:text-indigo-400
+            "
+          >
+            Invite People
+            <UserPlus className="ml-auto h-4 w-4" />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem className="cursor-pointer px-3 py-2 text-sm">
+            Server Settings
+            <Settings className="ml-auto h-4 w-4" />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem className="cursor-pointer px-3 py-2 text-sm">
+            Manage Members
+            <Users className="ml-auto h-4 w-4" />
+          </DropdownMenuItem>
+        )}
+        {isModerator && (
+          <DropdownMenuItem className="cursor-pointer px-3 py-2 text-sm">
+            Create Channel
+            <PlusCircle className="ml-auto h-4 w-4" />
+          </DropdownMenuItem>
+        )}
+
+        {isModerator && <DropdownMenuSeparator />}
+
+        {isAdmin && (
+          <DropdownMenuItem className="cursor-pointer px-3 py-2 text-sm text-rose-500">
+            Delete Server
+            <Trash className="ml-auto h-4 w-4" />
+          </DropdownMenuItem>
+        )}
+        {!isAdmin && (
+          <DropdownMenuItem className="cursor-pointer px-3 py-2 text-sm text-rose-500">
+            Leave Server
+            <LogOut className="ml-auto h-4 w-4" />
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
