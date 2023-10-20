5d6e7ae4b4a091864b98bdec481d5a64f4bbb9c0
John Soto <soto.ja415@gmail.com>
2023-10-20 18:20:12 -0400

    Added shadcn command

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Search } from "lucide-react";
+
 type ServerSearchProps = {
   data: {
     label: string;
@@ -14,7 +16,61 @@ type ServerSearchProps = {
   }[];
 };
 const ServerSearch = ({ data }: ServerSearchProps) => {
-  return <div>Server Search</div>;
+  return (
+    <>
+      <button
+        className="
+          group
+          flex
+          w-full
+          items-center
+          gap-x-2
+          rounded-md
+          px-2
+          py-2
+          transition
+          hover:bg-zinc-700/10
+          dark:hover:bg-zinc-700/50
+         "
+      >
+        <Search className="h-4 w-4 text-zinc-500 dark:text-zinc-400" />
+        <span
+          className="
+            text-sm
+            font-semibold
+            text-zinc-500
+            transition
+            group-hover:text-zinc-600
+            dark:text-zinc-400
+            dark:group-hover:text-zinc-300
+          "
+        >
+          Search
+        </span>
+        <kbd
+          className="
+              pointer-events-none
+              ml-auto
+              inline-flex
+              h-5
+              select-none
+              items-center
+              gap-1
+              rounded
+              border
+              bg-muted
+              px-1.5
+              font-mono
+              text-[10px]
+              font-medium
+              text-muted-foreground          
+            "
+        >
+          <span className="text-sm">CTRL</span>K
+        </kbd>
+      </button>
+    </>
+  );
 };
 
 export default ServerSearch;
