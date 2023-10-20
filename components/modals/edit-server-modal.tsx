60ccf848ef15f53420b1915e4cdebed57fd9ec53
John Soto <soto.ja415@gmail.com>
2023-10-19 22:15:35 -0400

    removed className

--- components/modals/edit-server-modal.tsx
+++ components/modals/edit-server-modal.tsx
@@ -152,7 +152,7 @@ const EditServerModal = () => {
             </div>
 
             <DialogFooter className="bg-gray-100 px-6 py-4">
-              <Button variant="primary" disabled={isLoading} className="w-full">
+              <Button variant="primary" disabled={isLoading}>
                 Save
               </Button>
             </DialogFooter>
