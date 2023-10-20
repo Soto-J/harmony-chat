b9728226a25df9ac8f242250e020d275b582b7e2
John Soto <soto.ja415@gmail.com>
2023-10-20 17:18:43 -0400

    Added channel data and serverSearch component

---
+++ components/server/server-search.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+type ServerSearchProps = {
+  data: {
+    label: string;
+    type: "channel" | "member";
+    data:
+      | {
+          icon: React.ReactNode;
+          name: string;
+          id: string;
+        }[]
+      | undefined;
+  }[];
+};
+const ServerSearch = ({ data }: ServerSearchProps) => {
+  return <div>Server Search</div>;
+};
+
+export default ServerSearch;
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,11 +1,30 @@
-import { currentProfile } from "@/lib/current-profile";
-import { db } from "@/lib/db";
-import { ChannelType } from "@prisma/client";
 import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+import { ChannelType, MemberRole } from "@prisma/client";
+import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
+
 import ServerHeader from "./server-header";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import ServerSearch from "./server-search";
 
 type ServerSidebarProps = {
   serverId: string;
+};
+
+const iconMap = {
+  [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4" />,
+  [ChannelType.VOICE]: <Mic className="mr-2 h-4 w-4" />,
+  [ChannelType.VIDEO]: <Video className="mr-2 h-4 w-4" />,
+};
+
+const roleIconMap = {
+  [MemberRole.ADMIN]: <ShieldAlert className="mr-2 h-4 w-4 text-indigo-500" />,
+  [MemberRole.MODERATOR]: (
+    <ShieldCheck className="mr-2 h-4 w-4 text-indigo-500" />
+  ),
+  [MemberRole.GUEST]: null,
 };
 
 const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
@@ -62,6 +81,51 @@ const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
       "
     >
       <ServerHeader server={server} role={profileRole} />
+
+      <ScrollArea className="flex-1 px-3">
+        <div className="mt-2">
+          <ServerSearch
+            data={[
+              {
+                label: "Text Channel",
+                type: "channel",
+                data: textChannels.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Vocie Channel",
+                type: "channel",
+                data: voiceChannels.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Vocie Channel",
+                type: "channel",
+                data: videoChannels.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Members",
+                type: "channel",
+                data: members.map((member) => ({
+                  id: member.id,
+                  name: member.profile.name,
+                  icon: roleIconMap[member.role],
+                })),
+              },
+            ]}
+          />
+        </div>
+      </ScrollArea>
     </div>
   );
 };
