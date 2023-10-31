447b80c366c89c72ca1a2823a00a0d744428b76c
John Soto <soto.ja415@gmail.com>
2023-10-31 19:20:19 -0400

    Added ChatHeader component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { redirect } from "next/navigation";
 
-const page = () => {
-  return <div>Channel ID Page</div>;
+import { redirectToSignIn } from "@clerk/nextjs";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+import ChatHeader from "@/components/chat/chat-header";
+
+type ChannelIdPageProps = {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+};
+
+const page = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: { id: params.channelId, profileId: profile.id },
+  });
+
+  const member = await db.member.findFirst({
+    where: { serverId: params.serverId, profileId: profile.id },
+  });
+
+  if (!channel || !member) {
+    return redirect("/");
+  }
+  return (
+    <div className="flex h-full flex-col bg-white dark:bg-[#313338]">
+      <ChatHeader />
+    </div>
+  );
 };
 
 export default page;
