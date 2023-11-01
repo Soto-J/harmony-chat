361c1a6bee10657fc34ad85b7c98c3910a08a66d
John Soto <soto.ja415@gmail.com>
2023-10-31 22:56:26 -0400

    Added MobileToggle nav

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,4 +1,5 @@
-import { Hash, Menu } from "lucide-react";
+import { Hash } from "lucide-react";
+import MobileToggle from "@/components/mobile-toggle";
 
 type ChatHeaderProps = {
   serverId: string;
@@ -11,20 +12,29 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
   return (
     <div
       className="
-      text-md 
-      flex 
-      h-12 
-      items-center
-      border-b-2
-      border-neutral-200
-      px-3
-      font-semibold
-      dark:border-neutral-800
+        text-md 
+        flex 
+        h-12 
+        items-center
+        border-b-2
+        border-neutral-200
+        px-3
+        font-semibold
+        dark:border-neutral-800
       "
     >
-      <Menu className="mr-2 h-6 w-6 md:hidden" />
+      <MobileToggle serverId={serverId} />
+
       {type === "channel" && (
-        <Hash className="mr-2 h-5 w-5 text-zinc-500 dark:text-zinc-400" />
+        <Hash
+          className="
+            mr-2 
+            h-5 
+            w-5 
+            text-zinc-500 
+            dark:text-zinc-400
+          "
+        />
       )}
       <p className="text-md font-semibold text-black dark:text-white">{name}</p>
     </div>
--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { Menu } from "lucide-react";
 
-const MobileToggle = () => {
-  return <div>MobileToggle</div>;
+import { Button } from "./ui/button";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import NavigationSidebar from "@/components/navigation/navigation-sidebar";
+import ServerSidebar from "./server/server-sidebar";
+
+type MobileToggleProps = {
+  serverId: string;
+};
+
+const MobileToggle = ({ serverId }: MobileToggleProps) => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+
+      <SheetContent side="left" className="flex gap-0 p-0">
+        <div className="w-[72px]">
+          <NavigationSidebar />
+        </div>
+        <ServerSidebar serverId={serverId} />
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileToggle;
